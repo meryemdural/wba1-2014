9d1c655adbfd66fd4db4d21213c65f3e9a5b3dd7
Volker Schaefer <schaefer.volker@googlemail.com>
2014-10-23 13:53:31 +0200

    added 'validate.js'

---
+++ 2014-10-23-javascript-advanced-uebung/material/js/validate.js
@@ -0,0 +1,21 @@
+ //Wird gefeuert wenn Seite geladen wurde
+ window.addEventListener('load', function () {
+
+ 	//Formular über ID selektieren und in Variable 'form speichern'
+	//TODO
+	
+ 	//wird gefeuert, wenn das Formular abgeschickt wurde.
+ 	form.addEventListener('submit', function (e) {
+		
+			//Daten im Formular validieren
+			//TODO
+		
+			// Default-Verhalten von 'submit' deaktivieren. 
+			//Soll nur ausgeführt werden, wenn die Daten im Formular nicht valide sind.
+			e.preventDefault();
+ 	});
+ });
+
+
+
+
