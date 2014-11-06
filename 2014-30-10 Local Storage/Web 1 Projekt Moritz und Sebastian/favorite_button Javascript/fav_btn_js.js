733fdf8baf607e64b53856847dc830f5f18f2537
Tarzan123 <lilmor12@nith.no>
2014-11-06 12:06:12 +0100

    fav button update
    fav button update, for loccal storage

---
+++ 2014-30-10 Local Storage/Web 1 Projekt Moritz und Sebastian/favorite_button Javascript/fav_btn_js.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function (){
+
+    $(".buyBtn1, .buyBtn2, .buyBtn3, .buyBtn4, .buyBtn5, .buyBtn6 ").after(
+        
+        "<input class='favorite_button' type='button' value='Favorisieren' />"
+        
+        
+        );
+    
+
+
+});
