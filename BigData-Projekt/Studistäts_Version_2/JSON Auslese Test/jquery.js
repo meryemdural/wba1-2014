544ea568b9a842901033f17b5eeba985a7480247
joelweih <mi1533@mimc35.gm.fh-koeln.de>
2014-11-06 16:29:18 +0100

    Jquery rumgespielt

--- BigData-Projekt/Studistäts_Version_2/JSON Auslese Test/jquery.js
+++ BigData-Projekt/Studistäts_Version_2/JSON Auslese Test/jquery.js
@@ -4,15 +4,16 @@ $(document).ready(function(){
 				  function(data){
 			$.getJSON("studienanfanger_studienfach_bundeslander.json",
 					 function(data2){
-//myDoughnut.segments[0].value = parseInt(data.semester1[1]["Deutsche Weiblich"]);			
-myDoughnut.segments[1].value = parseInt(data.semester1[1]["Deutsche Weiblich"])/parseInt(data2.semester[2]["Deutsche Insgesamt"]);
-//myDoughnut.segments[2].value = parseInt(data.semester1[1]["Deutsche Insgesamt"]);
-myDoughnut.segments[0].label = "Deutsche Weiblich";
-myDoughnut.segments[1].label = "Deutsche Männlich";
-myDoughnut.segments[2].label = "Deutsche Insgesamt";
-
-		myDoughnut.update();
-		});		
+                        //myDoughnut.segments[0].value = parseInt(data.semester1[1]["Deutsche Weiblich"]);			
+                        myDoughnut.segments[1].value = parseInt(data.semester1[1]["Deutsche Weiblich"])/parseInt(data2.semester[2]["Deutsche Insgesamt"]);
+                        //myDoughnut.segments[2].value = parseInt(data.semester1[1]["Deutsche Insgesamt"]);
+                        myDoughnut.segments[0].label = "Deutsche Weiblich";
+                        myDoughnut.segments[1].label = "Deutsche Männlich";
+                        myDoughnut.segments[2].label = "Deutsche Insgesamt";
+                myDoughnut.update();
+		
+		  });		
 		});
-	});
+	
+    });
 });
