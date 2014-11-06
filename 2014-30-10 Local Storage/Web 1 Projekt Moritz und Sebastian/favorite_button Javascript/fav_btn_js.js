41454d217280d6eed37244b20f1d7319ff23923a
Tarzan123 <lilmor12@nith.no>
2014-11-06 22:35:31 +0100

    Click event für die knøøpfe update und LOCAL STORE
    LOCAL STORAGE LOGIKK

--- 2014-30-10 Local Storage/Web 1 Projekt Moritz und Sebastian/favorite_button Javascript/fav_btn_js.js
+++ 2014-30-10 Local Storage/Web 1 Projekt Moritz und Sebastian/favorite_button Javascript/fav_btn_js.js
@@ -1,12 +1,19 @@
-﻿$(document).ready(function (){
+﻿$(document).ready(function () {
+
 
     $(".buyBtn1, .buyBtn2, .buyBtn3, .buyBtn4, .buyBtn5, .buyBtn6 ").after(
-        
-        "<input class='favorite_button' type='button' value='Favorisieren' />"
-        
-        
-        );
+
+      "<input class='favorite_button' type='button' value='Favorisieren' />");
+
+        var list = $(document.getElementsByClassName('favorite_button'));
+        $.each(list, function (index, data) {
+
+            $(this).click(function () {
+
+                alert("geklickd" + index + "");
+            });
+
+        });
+
     
-
-
 });
---
+++ 2014-30-10 Local Storage/Web 1 Projekt Moritz und Sebastian/local_storage_Javascript/storage.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+
+    if (localStorage.getItem('userName') === '' || localStorage.getItem('userName') === null) {
+
+        var uname = prompt('Enter Your Name');
+        localStorage.setItem('userName', uname);
+        document.getElementById(userName).innerHTML = uname;
+    
+    } else {
+        document.getElementById('idProfil').innerHTML = localStorage.getItem('userName');
+    }
+    
+});
