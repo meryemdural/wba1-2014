97ff20087ed1fe328f5df6547c53f7a0c84f505a
joelweih <joelweih@users.noreply.github.com>
2014-11-11 18:08:54 +0100

    Sortierung und neues Chart-Element
    DoughnutChart in Studiengänge verschoben +
    neue LineChart in Bundesländer hinzugefügt

---
+++ BigData-Projekt/Studistäts_Version_2/Statistik_LineChart.js
@@ -0,0 +1,31 @@
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+
+
+			function ChartActive2(){
+				var ctx2 = document.getElementById("chart-area2").getContext("2d");
+				window.myLineChart = new Chart(ctx2).Line(data, true);
+			};
