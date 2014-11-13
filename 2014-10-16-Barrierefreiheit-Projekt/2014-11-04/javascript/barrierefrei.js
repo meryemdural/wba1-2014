6cbf3b1366c36bfe4e8c570a520035c042ab4956
SabineHenke <henkesabine@Henkes-MacBook-Pro.local>
2014-11-13 15:14:34 +0100

    Ordner gelöscht

--- 2014-10-16-Barrierefreiheit-Projekt/2014-11-04/javascript/barrierefrei.js
+++
@@ -1,7 +0,0 @@
-function kontrast() {
-document.getElementsByTagName("h1")[0].style.fontSize = '140%';
-document.getElementsByTagName("h1")[0].style.padding = '5px';
-document.getElementsByTagName("h1")[0].style.paddingTop = '10px';
-document.getElementsByTagName("h2")[0].style.fontSize = '140%'; //nur die erste h2 funktioniert
-document.getElementsByName("navigation").style.fontSize = "120%"; //funktioniert nicht. wie wird der text angesprochen?
-}
