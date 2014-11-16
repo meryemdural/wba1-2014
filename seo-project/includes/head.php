096b4e941617b38038a626146c3bfe9607fb67de
doschuster <doschuster@gmail.com>
2014-11-16 17:29:57 +0100

    alles oben
    alles oben da draußen

---
+++ seo-project/includes/head.php
@@ -0,0 +1,12 @@
+<?php 
+ob_start("ob_gzhandler");
+?>
+<!DOCTYPE html>
+<html lang="de-DE">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="geo.placename" content="Germany" />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon" /> 
+        <link rel="stylesheet" href="../css/main.css" />
